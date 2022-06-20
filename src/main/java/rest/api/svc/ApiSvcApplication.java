45ecacced6aa79b7d013822beb40beacac981296
starseat <jw.lee@bridgetec.co.kr>
2022-06-20 15:24:49 +0900

    프로젝트 최초 등록

---
+++ src/main/java/rest/api/svc/ApiSvcApplication.java
@@ -0,0 +1,13 @@
+package rest.api.svc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiSvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/rest/api/svc/ApiSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package rest.api.svc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
