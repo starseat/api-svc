b68f5c676b4bf39182d42b4506daa3cd11e04b9b
starseat <jw.lee@bridgetec.co.kr>
2022-06-20 17:38:30 +0900

    sub module 중 실제 서비스를위한 샘플 프로젝트 추가 (gralde project)

---
+++ api-etc/src/main/java/rest/api/svc/ApiEtcApplication.java
@@ -0,0 +1,11 @@
+package rest.api.svc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiEtcApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiEtcApplication.class, args);
+    }
+}
